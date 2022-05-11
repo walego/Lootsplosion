006dd1960f7ada9fcb58b45aa310be3addb44e60
William Lego <william.a.lego11@gmail.com>
2022-05-11 11:22:11 -0400

    Added font-awesome

--- Lootsplosion.MVC/App_Start/BundleConfig.cs
+++ Lootsplosion.MVC/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Lootsplosion.MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/fontawesome.css",
                       "~/Content/site.css"));
         }
     }
